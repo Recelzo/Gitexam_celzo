e78e5f8a358ff6bc3677914ed4a501a6949c9304
Red <redcelzo2@gmail.com>
2025-03-26 16:18:40 +0800

    updated

--- red.java
+++ red.java
@@ -1,17 +1,23 @@
 function changeTableStyle() {
     let table = document.getElementById("studentTable");
+
+    if (!table) {
+        console.error("Table not found!");
+        return;
+    }
+
     table.style.border = "2px solid red";
     table.style.backgroundColor = "yellow";
 
     let ths = table.getElementsByTagName("th");
-    for (let th of ths) {
+    Array.from(ths).forEach(th => {
         th.style.backgroundColor = "red";
         th.style.color = "white";
-    }
+    });
 
     let tds = table.getElementsByTagName("td");
-    for (let td of tds) {
+    Array.from(tds).forEach(td => {
         td.style.backgroundColor = "lightyellow";
         td.style.color = "black";
-    }
+    });
 }
